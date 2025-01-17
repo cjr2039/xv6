e67974d1e170c8212d0dceb6f60dc19343bd6eae
cjr2039 <2039285355@qq.com>
2025-01-18 02:56:07 +0800

    完成Lab6第一个实验

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;       /* the thread's context , for switch*/
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context* old, struct context* new);
               
 void 
 thread_init(void)
@@ -60,7 +80,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    thread_switch(&t->context, &next_thread->context);// save old thread context and restore new thread context
+  } else//下一个runable的thread就是当前thread，意味只有一个thread，不用switch
     next_thread = 0;
 }
 
@@ -74,6 +95,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;// ra指向func,切换上下文后会从func开始执行
+  t->context.sp = (uint64)&t->stack[STACK_SIZE -1];// user-stack , sp指向栈顶的地址，取地址
 }
 
 void 
